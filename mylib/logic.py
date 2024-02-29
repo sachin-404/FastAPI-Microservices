8cfb32ad6d112e78e0647c94c1ea06104bb7d6c4
Sachin Kant <sachinkant404@gmail.com>
2024-02-29 13:23:32 +0000

    update cli functionalities

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,9 @@ def wiki(name="War Goddess", length=2):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    return wikipedia.search(name)
