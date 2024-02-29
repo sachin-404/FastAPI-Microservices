b7f7b7fef85881bcd19213abbb3c5ac120007da5
Sachin Kant <sachinkant404@gmail.com>
2024-02-29 13:58:20 +0000

    configure fastapi server

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki, wiki
 
-print(wiki())
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Wikipedia API. Call /search or /summary"}
+
+
+@app.get("/search/{name}")
+async def search(name: str):
+    """Page to search in Wikipedia"""
+
+    result = search_wiki(name)
+    return {"result": result}
+
+
+@app.get("/summary/{name}")
+async def wiki_summary(name: str):
+    """Get summary from Wikipedia"""
+
+    result = wiki(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,7 +1,7 @@
 import wikipedia
 
 
-def wiki(name="War Goddess", length=2):
+def wiki(name, length=5):
     """This is a wikipedia fetcher"""
 
     my_wiki = wikipedia.summary(name, length)
