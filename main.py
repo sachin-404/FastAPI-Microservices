8fe1edfc0d2bf6fe066bd4914e50a2d3f9241944
Sachin Kant <sachinkant404@gmail.com>
2024-02-29 19:31:18 +0000

    added NLP

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
-from mylib.logic import search_wiki, wiki
+from mylib.logic import search_wiki, wiki, phrase, parts_of_speech_tags
 
 
 app = FastAPI()
@@ -24,6 +24,22 @@ async def wiki_summary(name: str):
     """Get summary from Wikipedia"""
 
     result = wiki(name)
+    return {"result": result}
+
+
+@app.get("/phrase/{name}")
+async def noun_phrases(name: str):
+    """Retrieve Wikipedia page and return phrases"""
+
+    result = phrase(name)
+    return {"result": result}
+
+
+@app.get("/pos/{name}")
+async def part_of_speech(name: str):
+    """Retrieve Wikipedia page and return Part-of-speech tags"""
+
+    result = parts_of_speech_tags(name)
     return {"result": result}
 
 
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,4 +1,6 @@
 import wikipedia
+from textblob import TextBlob
+from collections import defaultdict
 
 
 def wiki(name, length=5):
@@ -12,3 +14,21 @@ def search_wiki(name):
     """Search Wikipedia for Names"""
 
     return wikipedia.search(name)
+
+
+def phrase(name):
+    """Returns phrases from wikipedia"""
+
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
+
+
+def parts_of_speech_tags(name):
+    """Returns parts of speech of the response"""
+
+    blob = TextBlob(wiki(name))
+    tags_dict = defaultdict(list)
+    for i, j in blob.tags:
+        tags_dict[j].append(i)
+    return tags_dict
